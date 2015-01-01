84c9ce24379804ce38770882596e0e52f7871fed
Jonathan <Jonathan@Jonathan-PC>
2015-01-01 11:07:13 -0500

    Refactor

--- PokerRule/PokerRule.py
+++ PokerRule/PokerRule.py
@@ -4,22 +4,21 @@ Created on 2014-12-31
 @author: Jonathan
 '''
 from sklearn.ensemble import RandomForestClassifier
-from numpy import genfromtxt, savetxt
 import numpy as np
 
 
-def main():
+def random_forest():
     # create the training & test sets, skipping the header row with [1:]
-    dataset = genfromtxt(open('Data/train.csv', 'r'),
-                         delimiter=',',
-                         dtype='f8')[1:]
+    dataset = np.genfromtxt(open('Data/train.csv', 'r'),
+                            delimiter=',',
+                            dtype='f8')[1:]
     target = [x[-1] for x in dataset]
     train = [x[:-1] for x in dataset]
     print len([x for x in target if x == 0])
     print len([x for x in target if x == 1])
-    test = genfromtxt(open('Data/test.csv', 'r'),
-                      delimiter=',',
-                      dtype='f8')[1:]
+    test = np.genfromtxt(open('Data/test.csv', 'r'),
+                         delimiter=',',
+                         dtype='f8')[1:]
     test_hands = [x[1:] for x in test]
     '''
     create and train the random forest
@@ -30,8 +29,13 @@ def main():
     rf.fit(train, target)
     result = rf.predict(test_hands)
     to_submit = np.column_stack((range(1, len(result) + 1), result))
-    savetxt('Data/submission3.csv', to_submit,
-            delimiter=',', fmt='%d')
+    np.savetxt('Data/submission3.csv', to_submit,
+               delimiter=',', fmt='%d')
+
+
+def main():
+    random_forest()
+
 
 if __name__ == "__main__":
     main()
